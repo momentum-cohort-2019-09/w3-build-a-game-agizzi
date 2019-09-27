30cfca4b805f9c472e5bdb4f3ee026241ef90c28
agizzi <alexandra.n.gizzi@gmail.com>
2019-09-27 17:34:30 -0400

    right key issue

--- game.js
+++ game.js
@@ -17,10 +17,23 @@ class Game {
 			y: 296
 		};
 		this.addBody(new Player(playerSize, playerStartLocation));
+
+		let tick = () => {
+			this.update();
+			this.draw(this.screen, this.size);
+			requestAnimationFrame(tick);
+		};
+		tick();
 	}
 
 	addBody(body) {
 		this.bodies.push(body);
+	}
+
+	update() {
+		for (let body of this.bodies) {
+			body.update();
+		}
 	}
 
 	play() {
@@ -36,6 +49,7 @@ class Game {
 		this.screen.strokeRect(150, 150, 200, 200);
 
 		for (let body of this.bodies) {
+			body.update(this);
 			body.draw(this.screen);
 		}
 	}
@@ -45,13 +59,56 @@ class Player {
 	constructor(size, position) {
 		this.size = size;
 		this.position = position;
+		this.keyboard = new Keyboarder();
 	}
-	// update (game) {}
+	update(game) {}
 	draw(screen) {
 		screen.fillStyle = 'white';
 		screen.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
 	}
+
+	update() {
+		if (this.keyboard.isDown(Keyboarder.KEYS.RIGHT)) {
+			this.position.x += 64;
+		}
+		// console.log(this.position.x);
+	}
 }
+// }
+
+class Keyboarder {
+	constructor() {
+		this.keyState = {};
+
+		window.addEventListener(
+			'keydown',
+			function(e) {
+				this.keyState[e.keyCode] = true;
+			}.bind(this)
+		);
+
+		window.addEventListener(
+			'keyup',
+			function(e) {
+				this.keyState[e.KeyCode] = false;
+			}.bind(this)
+		);
+	}
+
+	isDown(keyCode) {
+		return this.keyState[keyCode] === true;
+	}
+
+	on(keyCode, callback) {
+		window.addEventListener('keydown', function(e) {
+			if (e.keyCode === keyCode) {
+				callback();
+			}
+		});
+	}
+}
+
+Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, S: 83 };
 
 const game = new Game('gameCanvas');
 game.play();
