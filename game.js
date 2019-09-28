3343c9299ce9589c247656b26d567368276be9ef
agizzi <alexandra.n.gizzi@gmail.com>
2019-09-28 16:07:37 -0400

    udate

--- game.js
+++ game.js
@@ -37,20 +37,25 @@ class Game {
 	}
 
 	update() {
-		let notCollidingWithAnything = (b1) => {
-			return (
-				this.bodies.filter(function(b2) {
-					return collidingCoin(b1, b2);
-				}).length === 0
-			);
-		};
-
-		this.bodies = this.bodies.filter(notCollidingWithAnything);
-
-		for (let i = 0; i < this.bodies.length; i++) {
-			this.bodies[i].update();
-		}
+		// for (let body of this.bodies) {
+		// 	this.body.update();
 	}
+
+	// update() {
+	// 	let notCollidingWithAnything = (b1) => {
+	// 		return (
+	// 			this.bodies.filter(function(b2) {
+	// 				return collidingCoin(b1, b2);
+	// 			}).length === 0
+	// 		);
+	// 	};
+
+	// 	this.bodies = this.bodies.filter(notCollidingWithAnything);
+
+	// 	for (let i = 0; i < this.bodies.length; i++) {
+	// 		this.bodies[i].update();
+	// 	}
+	// }
 
 	play() {
 		const tick = () => {
@@ -103,7 +108,6 @@ class Player {
 		if (this.keyboard.isDown(Keyboarder.KEYS.LEFT)) {
 			// this.movement = true;
 			this.position.x -= 64;
-
 			if (this.position.x <= 160) {
 				this.position.x = 160;
 			}
@@ -111,7 +115,6 @@ class Player {
 		if (this.keyboard.isDown(Keyboarder.KEYS.UP)) {
 			// this.movement = true;
 			this.position.y -= 64;
-
 			if (this.position.y <= 160) {
 				this.position.y = 160;
 			}
@@ -119,7 +122,6 @@ class Player {
 		if (this.keyboard.isDown(Keyboarder.KEYS.DOWN)) {
 			// this.movement = true;
 			this.position.y += 64;
-
 			if (this.position.y >= 296) {
 				this.position.y = 296;
 			}
@@ -158,10 +160,7 @@ class Keyboarder {
 		window.addEventListener(
 			'keyup',
 			function(e) {
-				console.log('keyup')
-				console.log(e.keycode + this.keyState[e.KeyCode])
 				this.keyState[e.KeyCode] = false;
-				console.log(e.keycode + this.keyState[e.KeyCode])
 			}.bind(this)
 		);
 	}
@@ -169,10 +168,6 @@ class Keyboarder {
 	isDown(keyCode) {
 		return this.keyState[keyCode] === true;
 	}
-
-	// isUp(keyCode) {
-	// 	return this.keyState[keyCode] === false;
-	// }
 
 	on(keyCode, callback) {
 		window.addEventListener('keydown', function(e) {
@@ -185,26 +180,26 @@ class Keyboarder {
 
 Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40 };
 
-function collidingSweeper(b1, b2) {
-	return !(
-		b1 === b2 ||
-		b1.size.width / 2 < b2.size.width / 2 ||
-		b1.size.height / 2 < b2.size.height / 2 ||
-		b1.size.width / 2 > b2.size.width / 2 ||
-		b1.size.height / 2 > b2.size.height / 2
-	);
-}
+// function collidingSweeper(b1, b2) {
+// 	return !(
+// 		b1 === b2 ||
+// 		b1.size.width / 2 < b2.size.width / 2 ||
+// 		b1.size.height / 2 < b2.size.height / 2 ||
+// 		b1.size.width / 2 > b2.size.width / 2 ||
+// 		b1.size.height / 2 > b2.size.height / 2
+// 	);
+// }
 
-function collidingCoin(b1, b2) {
-	console.log(b1, b2);
-	return !(
-		b1 === b2 &&
-		b1.size.width < b2.size.width &&
-		b1.size.height < b2.size.height &&
-		b1.size.width > b2.size.width &&
-		b1.size.height > b2.size.height
-	);
-}
+// function collidingCoin(b1, b2) {
+// 	console.log(b1, b2);
+// 	return !(
+// 		b1 === b2 &&
+// 		b1.size.width < b2.size.width &&
+// 		b1.size.height < b2.size.height &&
+// 		b1.size.width > b2.size.width &&
+// 		b1.size.height > b2.size.height
+// 	);
+// }
 
 const game = new Game('gameCanvas');
 game.play();
