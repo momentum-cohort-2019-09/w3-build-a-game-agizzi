1be4145a305c102ec63f1c4fa51d7b0849cd338e
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2019-09-28 15:03:27 -0400

    add console logs

--- game.js
+++ game.js
@@ -84,14 +84,18 @@ class Player {
 		if (this.keyboard.isDown(Keyboarder.KEYS.RIGHT)) {
 			this.movement = true;
 			this.position.x += 64;
+			console.log("moved right " + this.position.x)
 
 			if (this.position.x >= 296) {
 				this.position.x = 296;
 			}
 		}
 		if (this.keyboard.isDown(Keyboarder.KEYS.LEFT)) {
+			console.log()
 			this.movement = true;
 			this.position.x -= 64;
+			console.log("moved left " + this.position.x)
+
 			if (this.position.x <= 160) {
 				this.position.x = 160;
 			}
@@ -99,6 +103,8 @@ class Player {
 		if (this.keyboard.isDown(Keyboarder.KEYS.UP)) {
 			this.movement = true;
 			this.position.y -= 64;
+			console.log("moved up " + this.position.y)
+
 			if (this.position.y <= 160) {
 				this.position.y = 160;
 			}
@@ -106,6 +112,8 @@ class Player {
 		if (this.keyboard.isDown(Keyboarder.KEYS.DOWN)) {
 			this.movement = true;
 			this.position.y += 64;
+			console.log("moved down " + this.position.y)
+
 			if (this.position.y >= 296) {
 				this.position.y = 296;
 			}
@@ -138,6 +146,7 @@ class Keyboarder {
 			'keydown',
 			function(e) {
 				this.keyState[e.keyCode] = true;
+				console.log(e.KeyCode + " is down")
 			}.bind(this)
 		);
 
@@ -145,17 +154,20 @@ class Keyboarder {
 			'keyup',
 			function(e) {
 				this.keyState[e.KeyCode] = false;
+				console.log(e.KeyCode + " is up")
 			}.bind(this)
 		);
 	}
 
 	isDown(keyCode) {
+		console.log(this.keyState[keyCode] + keyCode +" is down")
 		return this.keyState[keyCode] === true;
 	}
 
 	on(keyCode, callback) {
 		window.addEventListener('keydown', function(e) {
 			if (e.keyCode === keyCode) {
+				console.log(callback)
 				callback();
 			}
 		});
