5f96fc4b839ba25fe8d5b2ef39901bd3badfdcf6
agizzi <alexandra.n.gizzi@gmail.com>
2019-09-29 18:21:54 -0400

    collision is undefined

--- game.js
+++ game.js
@@ -4,60 +4,23 @@ class Game {
 		this.screen = canvas.getContext('2d');
 		this.size = { width: canvas.width, height: canvas.height };
 		this.bodies = [];
+		// this.coin = coin;
+		// this.player = player;
 
-		let playerSize = {
-			width: 44,
-			height: 44
-		};
-
-		let playerStartLocation = {
-			x: 160,
-			y: 296
-		};
-
-		this.player = new Player(playerSize, playerStartLocation);
-		this.addBody(this.player);
-
-		let coinSize = {
-			width: 22,
-			height: 22
-		};
-
-		let coinStartLocation = {
-			x: 310,
-			y: 168
-		};
-
-		this.coin = new Coin(coinSize, coinStartLocation);
-		this.addBody(this.coin);
+		// this.score = 0;
 	}
 
 	addBody(body) {
 		this.bodies.push(body);
+		console.log(this.bodies);
 	}
-
-	update() {
-		// for (let body of this.bodies) {
-		// 	this.body.update();
-	}
-
-	// update() {
-	// 	let notCollidingWithAnything = (b1) => {
-	// 		return (
-	// 			this.bodies.filter(function(b2) {
-	// 				return collidingCoin(b1, b2);
-	// 			}).length === 0
-	// 		);
-	// 	};
-
-	// 	this.bodies = this.bodies.filter(notCollidingWithAnything);
-
-	// 	for (let i = 0; i < this.bodies.length; i++) {
-	// 		this.bodies[i].update();
-	// 	}
-	// }
 
 	play() {
+		let player = new Player(this);
+		this.addBody(player);
+		let coin = new Coin(this);
+		this.addBody(coin);
+
 		const tick = () => {
 			this.update();
 			this.draw(this.screen, this.size);
@@ -80,70 +43,116 @@ class Game {
 			body.draw(this.screen);
 		}
 	}
+
+	update() {}
 }
 
 class Player {
-	constructor(size, position) {
-		this.size = size;
-		this.position = position;
+	constructor(game, size) {
+		this.game = game;
+		this.size = { x: 44, y: 44 };
+		this.position = {
+			x: 160,
+			y: 296
+		};
+		this.center = { x: this.position.x / 2, y: this.position.y / 2 };
 		this.keyboard = new Keyboarder();
-		// this.movement = false;
 	}
 
 	draw(screen) {
 		screen.fillStyle = 'white';
-		screen.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+		screen.fillRect(this.position.x, this.position.y, 44, 44);
 	}
 
 	update() {
 		if (this.keyboard.isDown(Keyboarder.KEYS.RIGHT)) {
-			// this.movement = true;
-			this.position.x += 64;
+			this.position.x += 8;
 
 			if (this.position.x >= 296) {
 				this.position.x = 296;
 			}
-			// this.keyState === false;
 		}
 		if (this.keyboard.isDown(Keyboarder.KEYS.LEFT)) {
-			// this.movement = true;
-			this.position.x -= 64;
+			this.position.x -= 8;
 			if (this.position.x <= 160) {
 				this.position.x = 160;
 			}
 		}
 		if (this.keyboard.isDown(Keyboarder.KEYS.UP)) {
-			// this.movement = true;
-			this.position.y -= 64;
+			this.position.y -= 8;
 			if (this.position.y <= 160) {
 				this.position.y = 160;
 			}
 		}
 		if (this.keyboard.isDown(Keyboarder.KEYS.DOWN)) {
-			// this.movement = true;
-			this.position.y += 64;
+			this.position.y += 8;
 			if (this.position.y >= 296) {
 				this.position.y = 296;
 			}
 		}
-
-		// this.movement = false;
 		// console.log(this.position.x);
 	}
 }
 
 class Coin {
-	constructor(size, position) {
-		this.size = size;
-		this.position = position;
+	constructor(game, size, position) {
+		this.size = { x: 22, y: 22 };
+		this.position = { x: 307, y: 171 };
+		this.center = { x: this.position.x / 2, y: this.position.y / 2 };
+		this.collision = false;
 	}
 
 	draw(screen) {
 		screen.fillStyle = 'yellow';
-		screen.fillRect(this.position.x, this.position.y, this.size.width, this.size.height);
+		screen.fillRect(this.position.x, this.position.y, 22, 22);
 	}
 
-	update() {}
+	coinSpawn = [
+		{ x: 182, y: 182 },
+		{ x: 246, y: 182 },
+		{ x: 310, y: 182 },
+		{ x: 182, y: 246 },
+		{ x: 246, y: 246 },
+		{ x: 310, y: 246 },
+		{ x: 182, y: 310 },
+		{ x: 246, y: 310 },
+		{ x: 310, y: 310 }
+	];
+
+	// collision = false;
+
+	// coinCollision() {
+	// 	if (this.coin.position.x - Game.player.position.x <= 22) {
+	// 		collision = true;
+	// 	}
+	// }
+
+	update() {
+		Math.floor(Math.random() * Math.floor(9));
+		if (this.coin.position.x - Game.player.position.x <= 22) {
+			collision = true;
+		}
+		// console.log(this.position.x);
+		console.log(collision);
+	}
+}
+
+function colliding(b1, b2) {
+	return !// b1.safe ||
+	// b2.safe ||
+	(
+		b1 === b2 ||
+		b1.center.x + b1.size.width / 2 < b2.center.x - b2.size.width / 2 ||
+		b1.center.y + b1.size.height / 2 < b2.center.y - b2.size.height / 2 ||
+		b1.center.x - b1.size.width / 2 > b2.center.x + b2.size.width / 2 ||
+		b1.center.y - b1.size.height / 2 > b2.center.y + b2.size.height / 2
+	);
+}
+
+function drawScore() {
+	this.screen.font = '16px Century-Gothic';
+	this.screen.fillStyle = '#7EE8FA';
+	// this.screen.fillText('Score: ' + score);
 }
 
 class Keyboarder {
@@ -160,7 +169,7 @@ class Keyboarder {
 		window.addEventListener(
 			'keyup',
 			function(e) {
-				this.keyState[e.KeyCode] = false;
+				this.keyState[e.keyCode] = false;
 			}.bind(this)
 		);
 	}
@@ -180,26 +189,10 @@ class Keyboarder {
 
 Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40 };
 
-// function collidingSweeper(b1, b2) {
-// 	return !(
-// 		b1 === b2 ||
-// 		b1.size.width / 2 < b2.size.width / 2 ||
-// 		b1.size.height / 2 < b2.size.height / 2 ||
-// 		b1.size.width / 2 > b2.size.width / 2 ||
-// 		b1.size.height / 2 > b2.size.height / 2
-// 	);
-// }
+// window.addEventListener('load', function() {
+// 	let game = new Game('gameCanvas');
+// 	game.play();
+// });
 
-// function collidingCoin(b1, b2) {
-// 	console.log(b1, b2);
-// 	return !(
-// 		b1 === b2 &&
-// 		b1.size.width < b2.size.width &&
-// 		b1.size.height < b2.size.height &&
-// 		b1.size.width > b2.size.width &&
-// 		b1.size.height > b2.size.height
-// 	);
-// }
-
-const game = new Game('gameCanvas');
+let game = new Game('gameCanvas');
 game.play();
